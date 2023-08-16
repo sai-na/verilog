61ead2f3a9503ff46b4a30d58aa37b61020e91d7
sai-na <saipkmm100@gmail.com>
2023-08-16 23:30:03 +0530

    Update mux_tb_gpt.v

--- 11.mux/mux_tb_gpt.v
+++ 11.mux/mux_tb_gpt.v
@@ -1,26 +1,18 @@
-// Include the mux.v module
-include "mux_gpt.v"
 
-// Define the testbench module
+
+`include "mux_gpt.v"
+
 module muxTest;
-reg A, B, C, D, sel1, sel2;
+reg A,B,C,D,sel1,sel2;
 wire Y;
-
-// Instantiate the Mux module
-Mux i (Y, A, B, C, D, sel1, sel2);
-
-// Initialize inputs and observe output
+Mux i (Y,A,B,C,D,sel1,sel2);
 initial begin
-  A = 1; B = 0; C = 0; D = 0; sel1 = 0; sel2 = 0;
-  #1 A = 0; B = 1; C = 0; D = 0; sel1 = 0; sel2 = 1;
-  #1 A = 0; B = 0; C = 1; D = 0; sel1 = 1; sel2 = 0;
-  #1 A = 0; B = 0; C = 0; D = 1; sel1 = 1; sel2 = 1;
+    A=1;B=0;C=0;D=0;sel1=0;sel2=0;
+    #1 A=0;B=1;C=0;D=0;sel1=0;sel2=1;
+    #1 A=0;B=0;C=1;D=0;sel1=1;sel2=0;
+    #1 A=0;B=0;C=0;D=1;sel1=1;sel2=1;
 end
-
-// Display and monitor the values
 initial begin
-  $monitor("%t A=%d B=%d C=%d D=%d SEL1=%d SEL2=%d Output=%d",
-           $time, A, B, C, D, sel1, sel2, Y);
+    $monitor("%t A=%d B=%d C=%d D=%d SEL1=%d SEL2=%d Output=%d",$time,A,B,C,D,sel1,sel2,Y);
 end
-
 endmodule
